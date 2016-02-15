3ac7486253d7a00109d0194ebcc6bbd4ac461e93
FionaPopp <fionapopp@dhcp-27-54.hig.secure.int.wireless.unca.edu>
2016-02-14 21:33:02 -0500

    edited gripper file

--- Gripper/Gripper.ino
+++ Gripper/Gripper.ino
@@ -1,7 +1,7 @@
 // include the library code:
 #include <LiquidCrystal.h>
 #include <Wire.h> 
-#include "Adafruit_TCS34725.h"
+#include "Adafruit_TCS34725.h" // for color sensing
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
@@ -10,8 +10,44 @@ Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS347
 ///////////////////////////////////////////END OF INITIALIZATION///////////////////////////////////////////////////
 
 class Gripper {  /////////////////////////GRIPPER CLASS DEFINITION//////////////////////////////////////////////////
-  
+  public:
+  char block_1;
+  char block_2;
+  char block_3;
+  uint16_t colorTemp;
+
+  //Block positions declarations (Picture below shows block numbers)
+  // |1|    |3|
+  // | |    |2|
+  //      bot side of gripper
+
+  void getColor() {
+  //take color reading  
+  tcs.getRawData(&r, &g, &b, &c);
+  colorTemp = tcs.calculateColorTemperature(r, g, b);
+    
+    //red
+    if ((colorTemp > 4500) && (colorTemp < 9000)) {   
+    }
+    
+    //blue
+    else if ((colorTemp > 9000) && ( colorTemp < 13000)){
+    }
+    
+    //green
+    else if ((colorTemp > 3000) && (colorTemp < 4500)){
+    }
+    
+    //yellow
+    else if ( (colorTemp > 2000) && ( colorTemp < 3000 )){
+    }
+    
+    //ambient
+    else {
+     }
 }
+  }
+};
 //inititate grippers:////////////////////////////////////////////////////////////////////////////////////////
 
 Gripper A;
